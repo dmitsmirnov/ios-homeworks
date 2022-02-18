b243d0bc0a79a3c35fd9d50e38c5d44355c47e3d
dmitsmirnov <dmitsmirnov@bk.ru>
2022-02-18 17:27:40 +0300

    deleted comment code

--- Navigation/TabBarController.swift
+++ Navigation/TabBarController.swift
@@ -12,28 +12,15 @@ class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        self.viewControllers?.enumerated().forEach({(index, viewController) in
-//            switch index {
-//            case 0:
-//                viewController.tabBarItem.title = "Профиль"
-//            case 1:
-//                viewController.tabBarItem.title = "Лента"
-//            default:
-//                viewController.tabBarItem.title = ""
-//            }
-//        } )
-        
         let myTabBarItem1 = (self.tabBar.items?[0])! as UITabBarItem
         myTabBarItem1.image = UIImage(systemName: "star")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
         myTabBarItem1.selectedImage = UIImage(systemName: "star.fill")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
         myTabBarItem1.title = "Профиль"
-        //myTabBarItem1.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
         
         let myTabBarItem2 = (self.tabBar.items?[1])! as UITabBarItem
         myTabBarItem2.image = UIImage(systemName: "book")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
         myTabBarItem2.selectedImage = UIImage(systemName: "book.fill")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
         myTabBarItem2.title = "Лента"
-        //myTabBarItem2.imageInsets = UIEdgeInsets(top: 6, left: 10, bottom: -6, right: 10)
         
     }
     
